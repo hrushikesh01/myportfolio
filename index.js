53e1af3d543c9d59be61e84ecd68751b5769f279
hrushikesh01 <166029552+hrushikesh01@users.noreply.github.com>
2025-06-18 14:25:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-
-const words = ["UI/UX Designer", "Web Developer", "Software Developer", "Data Analyst"];
-let i = 0, j = 0, currentWord = "", isDeleting = false;
-
-function type() {
-  const typing = document.querySelector(".typing");
-
-  if (i < words.length) {
-    if (!isDeleting && j <= words[i].length) {
-      currentWord = words[i].slice(0, j++);
-    } else if (isDeleting && j >= 0) {
-      currentWord = words[i].slice(0, j--);
-    }
-
-    typing.textContent = currentWord;
-
-    if (j === words[i].length) {
-      isDeleting = true;
-      setTimeout(type, 2000);
-    } else if (j === 0 && isDeleting) {
-      isDeleting = false;
-      i = (i + 1) % words.length;
-    }
-
-    setTimeout(type, isDeleting ? 100 : 100);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", type);
-
 function toggleMenu() {
     const menu = document.getElementById("mobileMenu");
     menu.classList.toggle("show");
   }
-  
+  console.log("JS Loaded");
